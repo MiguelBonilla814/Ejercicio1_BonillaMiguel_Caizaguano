f0fef176372f44633b278f05390b4575ae7a19b9
Miguel Enrique Bonilla Quishpe <64868918+MiguelBonilla814@users.noreply.github.com>
2021-12-06 18:24:42 -0500

    Commit

--- app/src/main/java/com/example/ejercicio1_bonillamiguel_caizaguano/MainActivity.java
+++ app/src/main/java/com/example/ejercicio1_bonillamiguel_caizaguano/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.ejercicio1_bonillamiguel_caizaguano;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void AbrirActivityA(View view){
+        Intent intent = new Intent(this, EjercicioA_Activity.class);
+        startActivity(intent);
+    }
+
+    public void AbrirActivityB(View view){
+        Intent intent = new Intent(this, EjercicioB_Activity.class);
+        startActivity(intent);
     }
 }
