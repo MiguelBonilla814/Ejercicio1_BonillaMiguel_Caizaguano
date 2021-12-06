b0e934abfc2ed4eb81f856b852f97ce19441fc2c
kevho <kevincaizaguano2@gmail.com>
2021-12-06 18:56:07 -0500

    kevin caizaguano 18:55

--- app/src/main/java/com/example/ejercicio1_bonillamiguel_caizaguano/EjercicioB_Activity.java
+++ app/src/main/java/com/example/ejercicio1_bonillamiguel_caizaguano/EjercicioB_Activity.java
@@ -3,12 +3,36 @@ package com.example.ejercicio1_bonillamiguel_caizaguano;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class EjercicioB_Activity extends AppCompatActivity {
+
+    EditText editextValor1;
+    EditText editextValor2;
+    EditText editextResultado;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ejercicio_bactivity);
+
+        editextValor1 = findViewById(R.id.editTextTextValor1);
+        editextValor2 = findViewById(R.id.editTextValor2);
+        editextResultado = findViewById(R.id.editTextTextResultado);
+
     }
+
+    public void restar (View view) {
+         long numero1 = Long.parseLong(editextResultado.getText().toString());
+        long numero2 = Long.parseLong(editextResultado.getText().toString());
+
+        long  resultado = numero1 - numero2;
+
+        editextResultado.setText(String.valueOf(resultado));
+
+    }
+
+
 }
