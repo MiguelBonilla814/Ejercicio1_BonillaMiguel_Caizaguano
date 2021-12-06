ffc9d76d1a3250a87991185ca898d54472e62d56
Miguel Enrique Bonilla Quishpe <64868918+MiguelBonilla814@users.noreply.github.com>
2021-12-06 18:21:33 -0500

    Commit

---
+++ app/src/main/java/com/example/ejercicio1_bonillamiguel_caizaguano/EjercicioA_Activity.java
@@ -0,0 +1,14 @@
+package com.example.ejercicio1_bonillamiguel_caizaguano;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class EjercicioA_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ejercicio_aactivity);
+    }
+}
