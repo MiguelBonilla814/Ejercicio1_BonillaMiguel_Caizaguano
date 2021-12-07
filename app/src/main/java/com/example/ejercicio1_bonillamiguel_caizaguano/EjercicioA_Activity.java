6e4a4db1ccfcc7a00697cb5704a82c95ab6cd614
Miguel Enrique Bonilla Quishpe <64868918+MiguelBonilla814@users.noreply.github.com>
2021-12-06 19:04:07 -0500

    Miguel Bonill 19:03

--- app/src/main/java/com/example/ejercicio1_bonillamiguel_caizaguano/EjercicioA_Activity.java
+++ app/src/main/java/com/example/ejercicio1_bonillamiguel_caizaguano/EjercicioA_Activity.java
@@ -5,11 +5,14 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class EjercicioA_Activity extends AppCompatActivity {
 
     private EditText editText_valor1;
     private EditText editText_valor2;
+    private TextView textView_res;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -17,18 +20,34 @@ public class EjercicioA_Activity extends AppCompatActivity {
 
         editText_valor1 = findViewById(R.id.editText_valor1_suma);
         editText_valor2 = findViewById(R.id.editText_valor2_suma);
+        textView_res = findViewById(R.id.textView_resultado);
 
     }
 
     public void Suma(View view){
-        String v1, v2;
+        String v1, v2, espacio = "", resultado = "", data[];
         long n1, n2, suma;
+        data = new String[5];
         v1 = editText_valor1.getText().toString();
         v2 = editText_valor2.getText().toString();
         n1 = Long.parseLong(v1);
         n2 = Long.parseLong(v2);
+        data[0] = " ";
+        data[1] = v1;
+        data[2] = v2;
+        data[3] = "";
+        data[4] = "";
 
+        for (int i = 0; i < v2.length() + 2; i++){
+            data[3] += "-";
+        }
 
+        final int size = data.length;
+        for (int i = 0; i < size; i++)
+        {
+            resultado += (data[i] + "\n");
+        }
 
+        textView_res.setText(resultado);
     }
 }
