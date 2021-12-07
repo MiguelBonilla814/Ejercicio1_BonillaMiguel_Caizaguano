892f859a3585e45aca860d45fe8304813ba2357b
kevho <kevincaizaguano2@gmail.com>
2021-12-06 19:34:26 -0500

    kevin caizaguano 19:34

--- app/src/main/java/com/example/ejercicio1_bonillamiguel_caizaguano/EjercicioB_Activity.java
+++ app/src/main/java/com/example/ejercicio1_bonillamiguel_caizaguano/EjercicioB_Activity.java
@@ -11,6 +11,7 @@ public class EjercicioB_Activity extends AppCompatActivity {
     EditText editextValor1;
     EditText editextValor2;
     EditText editextResultado;
+    String result;
 
 
     @Override
@@ -25,14 +26,53 @@ public class EjercicioB_Activity extends AppCompatActivity {
     }
 
     public void restar (View view) {
-         long numero1 = Long.parseLong(editextResultado.getText().toString());
-        long numero2 = Long.parseLong(editextResultado.getText().toString());
+         long numero1 = Long.parseLong(editextValor1.getText().toString());
+        long numero2 = Long.parseLong(editextValor2.getText().toString());
 
         long  resultado = numero1 - numero2;
 
+        editextResultado.setText(String.valueOf(resultado));
 
 
     }
 
+/*
+    public static String minus(String firstNum, String secondNum){
+        result = "";
+        String sign = "";
+        if(firstNum.equals(secondNum)){
+            return "0";
+        }
+        // Número decimal-grande, intercambia firstNum y secondNum, y establece el signo en-
+        if(firstNum.length() < secondNum.length()
+                || (firstNum.length() == secondNum.length() && firstNum.compareTo(secondNum) < 0)){
+            sign = "-";
+            String tmp = firstNum;
+            firstNum = secondNum;
+            secondNum = tmp;
+        }
+        // Si el decimal (secondNum) no es suficiente, agregue 0 al orden superior
+        while(secondNum.length() < firstNum.length()){
+            secondNum = "0" + secondNum;
+        }
+        // Los números grandes comienzan desde el bit bajo y disminuyen el número poco a poco
+        int index = firstNum.length() - 1;
+        int préstamos = 0; // Grabar préstamos
+        while(index >= 0){
+            int left = firstNum.charAt(index) - secondNum.charAt(index) + borrow;
+            if(left < 0){
+                borrow = -1;
+                left += 10;
+            }
+            result = left + result;
+            index--;
+        }
+        // Eliminar el exceso de 0 inicial
+        while(result.indexOf("0") == 0){
+            result = result.substring(1);
+        }
+        return sign + result;
+    }
+*/
 
 }
